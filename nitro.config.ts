fd6d3fba22f47566d940c359cd2250fbdbbbf628
gaokefei <260480378@qq.com>
2022-07-06 14:43:29 +0800

    fix: Buffer base64

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,5 +2,4 @@ import { defineNitroConfig } from 'nitropack'
 
 export default defineNitroConfig({
   srcDir: './server',
-  preset: 'nitro-dev',
 })
--- server/api/user/login.post.ts
+++ server/api/user/login.post.ts
@@ -6,7 +6,7 @@ export default eventHandler(async (event) => {
   if (user?.password !== password)
     return createError({ statusCode: 401, message: '用户名或密码无效' })
 
-  const token = `Basic ${Buffer.from(`${username}:${password}`, 'base64').toString()}`
+  const token = `Basic ${Buffer.from(`${username}:${password}`, 'utf8').toString('base64')}`
   await useStorage().setItem(token, { ...user, permissions: [], timeout: Date.now() })
   return {
     data: token,
