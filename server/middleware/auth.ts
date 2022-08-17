89c69e634df8e9de7bfe474120ba3b46d54a0034
gaokefei <260480378@qq.com>
2022-08-17 10:33:12 +0800

    fix: resolve timeout undefined bug

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -10,7 +10,7 @@ export default defineEventHandler(async ({ req, context }) => {
   const userStore = await useStorage().getItem(req.headers.authorization)
   if (!userStore?.id || Date.now() - userStore.timeout > 30 * 60 * 1000) {
     // eslint-disable-next-line no-console
-    console.log(userStore?.id, Date.now(), userStore.timeout > 30 * 60 * 1000, '...debug')
+    console.log(userStore?.id, Date.now(), userStore?.timeout, '...debug')
     await useStorage().removeItem(req.headers.authorization)
     return createError({ statusCode: 401, message: '认证过期，请重新登陆' })
   }
