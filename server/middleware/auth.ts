9c0baf4fe024f8246bd5c25b090cdc6ee9fb593f
gaokefei <260480378@qq.com>
2022-08-17 10:09:25 +0800

    test: timeout error

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,28 +1,32 @@
 import { flatten, uniq } from 'lodash-es'
 import { getUserList } from '../api/users'
 
-const whiteList = ['/api/login', '/api/settings', '/api']
-
+const exclude = ['/api/login', '/api/settings', '/api']
 export default defineEventHandler(async ({ req, context }) => {
-  let permission = req.url?.replace(/^\/api([^?#]*).*$/, '$1')?.replace(/\d+/g, '[id]')
-  if (req.method !== 'GET')
-    permission += `/${req.method?.toLowerCase()}`
-  else if (permission.endsWith('/[id]'))
-    permission = permission.replace(/\/\[id\]$/, '')
-
-  if (whiteList.includes(req.url!))
+  // 不经过权限验证的路由
+  if (exclude.includes(req.url!))
     return
 
   const userStore = await useStorage().getItem(req.headers.authorization)
   if (!userStore?.id || Date.now() - userStore.timeout > 30 * 60 * 1000) {
+    console.log(userStore?.id, Date.now(), userStore.timeout > 30 * 60 * 1000, '...debug')
     await useStorage().removeItem(req.headers.authorization)
     return createError({ statusCode: 401, message: '认证过期，请重新登陆' })
   }
 
   const user = getUserList({ id: userStore.id })[0]
   const permissions = uniq(flatten(user.roles?.map((i: any) => i.permissions)))
+  let permission = req.url?.replace(/^\/api([^?#]*).*$/, '$1')?.replace(/\d+/g, '[id]')
+  if (req.method !== 'GET')
+    permission += `/${req.method?.toLowerCase()}`
+  else if (permission.endsWith('/[id]'))
+    permission = permission.replace(/\/\[id\]$/, '')
   if (req.url !== '/api/user-info' && !permissions.includes(permission))
     return createError({ statusCode: 403, message: '当前用户没有访问权限' })
 
-  await useStorage().setItem(req.headers.authorization, context.user = { ...user, permissions, timeout: Date.now() })
+  await useStorage().setItem(req.headers.authorization, context.user = {
+    ...user,
+    permissions,
+    timeout: Date.now(),
+  })
 })
